561149ba4e5beb8ac0c2f1de8013aaac8ef9ac9c
serhiy-grinvich <113624514+serhiy-grinvich@users.noreply.github.com>
2023-01-07 15:31:15 +0200

    add solution

---
+++ lesson1_intro/my_first_app/src/index.js
@@ -0,0 +1,16 @@
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootElement = document.querySelector('#root');
+
+const element = (
+  <div className="greeting">
+    <h1 className="greeting__title">Hello, world!</h1>
+    <p className="greeting__text">I'm learning React</p>
+  </div>
+);
+
+// console.log(greeting);
+
+ReactDOM.render(element, rootElement);
