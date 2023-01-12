10c0166defee8dbc364c492d41f5bd8c8cf9ba61
serhiy-grinvich <113624514+serhiy-grinvich@users.noreply.github.com>
2023-01-12 18:48:12 +0200

    fix solution

--- lesson3_components_props/task4_greeting_component/src/Greeting.jsx
+++ lesson3_components_props/task4_greeting_component/src/Greeting.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Greeting = (props) => {
   const age = new Date(Date.now() - props.birthDate).getFullYear() - 1970;
+  const { firstName, lastName } = props;
   // console.log(age);
 
   return (
-    <div className="greeting">{`My name is ${props.firstName} ${props.lastName}. I am ${age} years old`}</div>
+    <div className="greeting">{`My name is ${firstName} ${lastName}. I am ${age} years old`}</div>
   );
 };
 
