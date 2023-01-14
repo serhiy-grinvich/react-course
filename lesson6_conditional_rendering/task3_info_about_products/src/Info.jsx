7f66a371b78cbcaad19f07f83189fbb6e5f4dac9
serhiy-grinvich <113624514+serhiy-grinvich@users.noreply.github.com>
2023-01-14 23:20:01 +0200

    fix

--- lesson6_conditional_rendering/task3_info_about_products/src/Info.jsx
+++ lesson6_conditional_rendering/task3_info_about_products/src/Info.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Info = ({ message }) => {
   if (!message) return null;
-  return <div className="info">{message}</div>;
+  return <div className="message">{message}</div>;
 };
 
 export default Info;
