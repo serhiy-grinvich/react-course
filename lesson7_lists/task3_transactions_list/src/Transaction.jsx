1e5d4d5133fdfd99eff13305c41b1d4dfc696193
serhiy-grinvich <113624514+serhiy-grinvich@users.noreply.github.com>
2023-01-16 20:24:21 +0200

    fix

--- lesson7_lists/task3_transactions_list/src/Transaction.jsx
+++ lesson7_lists/task3_transactions_list/src/Transaction.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import moment from 'moment';
 
-const Transaction = ({ from, to, amount, rate, time, date }) => {
+const Transaction = ({ from, to, amount, rate, time /*date*/ }) => {
+  let formatter = new Intl.NumberFormat('en-GB');
+  const formattedAmount = formatter.format(amount);
+  const date = moment(time).format('D MMM');
+  const formattedTime = moment(time).locale('uk').format('LT');
   return (
     <li className="transaction">
       <span className="transaction__date">{date}</span>
-      <span className="transaction__time">{time}</span>
+      <span className="transaction__time">{formattedTime}</span>
       <span className="transaction__assets">{`${from} → ${to}`}</span>
       <span className="transaction__rate">{rate}</span>
-      <span className="transaction__amount">{amount}</span>
+      <span className="transaction__amount">{formattedAmount}</span>
     </li>
   );
 };
--- lesson7_lists/task3_transactions_list/src/TransactionsList.jsx
+++ lesson7_lists/task3_transactions_list/src/TransactionsList.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
 import Transaction from './Transaction';
-import moment from 'moment';
+// import moment from 'moment';
 
 class TransactionsList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sorting: null,
-    };
-  }
-
   render() {
-    let formatter = new Intl.NumberFormat('en-GB');
-    const transactions = this.props.transactions.map((transaction) => ({
-      ...transaction,
-      amount: formatter.format(transaction.amount),
-      date: moment(transaction.time).format('D MMM'),
-      time: moment(transaction.time).locale('uk').format('LT'),
-    }));
+    // let formatter = new Intl.NumberFormat('en-GB');
+    // const transactions = this.props.transactions.map((transaction) => ({
+    //   ...transaction,
+    //   amount: formatter.format(transaction.amount),
+    //   date: moment(transaction.time).format('D MMM'),
+    //   time: moment(transaction.time).locale('uk').format('LT'),
+    // }));
     // console.log(transactions);
     return (
       <ul className="transactions">
-        {transactions.map((transaction) => (
+        {this.props.transactions.map((transaction) => (
           <Transaction key={transaction.id} {...transaction} />
         ))}
       </ul>
