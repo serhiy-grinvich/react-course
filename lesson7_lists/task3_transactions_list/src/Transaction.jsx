fed2587282e732ec95eabac81ae01f331a59ae8b
serhiy-grinvich <113624514+serhiy-grinvich@users.noreply.github.com>
2023-01-17 16:55:25 +0200

    fix solution

--- lesson7_lists/task3_transactions_list/src/Transaction.jsx
+++ lesson7_lists/task3_transactions_list/src/Transaction.jsx
@@ -11,11 +11,9 @@ const Transaction = ({ from, to, amount, rate, time }) => {
   return (
     <li className="transaction">
       <span className="transaction__date">{moment(time).format('DD MMM')}</span>
-      <span className="transaction__time">
-        {moment(time).locale('en-gb').format('LT')}
-      </span>
+      <span className="transaction__time">{moment(time).format('HH:mm')}</span>
       <span className="transaction__assets">{`${from} → ${to}`}</span>
-      <span className="transaction__rate">{formatter.format(rate)}</span>
+      <span className="transaction__rate">{rate}</span>
       <span className="transaction__amount">{formatter.format(amount)}</span>
     </li>
   );
