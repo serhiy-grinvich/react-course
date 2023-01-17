9cefe7e77bd9ffd14f1576735434f6b4955b7ede
serhiy-grinvich <113624514+serhiy-grinvich@users.noreply.github.com>
2023-01-17 12:47:08 +0200

    fix

--- lesson7_lists/task3_transactions_list/src/TransactionsList.jsx
+++ lesson7_lists/task3_transactions_list/src/TransactionsList.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import Transaction from './Transaction';
-// import moment from 'moment';
+import moment from 'moment';
+
+let formatter = new Intl.NumberFormat('en-GB');
 
 class TransactionsList extends React.Component {
   render() {
-    // let formatter = new Intl.NumberFormat('en-GB');
-    // const transactions = this.props.transactions.map((transaction) => ({
-    //   ...transaction,
-    //   amount: formatter.format(transaction.amount),
-    //   date: moment(transaction.time).format('D MMM'),
-    //   time: moment(transaction.time).locale('en-gb').format('LT'),
-    // }));
-    // console.log(transactions);
+    const transactions = this.props.transactions.map((transaction) => ({
+      ...transaction,
+      amount: formatter.format(transaction.amount),
+      date: moment(transaction.time).format('DD MMM'),
+      time: moment(transaction.time).locale('en-gb').format('LT'),
+    }));
+    console.log(transactions);
     return (
       <ul className="transactions">
-        {this.props.transactions.map((transaction) => (
+        {transactions.map((transaction) => (
           <Transaction key={transaction.id} {...transaction} />
         ))}
       </ul>
