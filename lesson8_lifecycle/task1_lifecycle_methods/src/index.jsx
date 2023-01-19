bd54d4ae05adb8a556121f6ad6f51af7e44bfeeb
serhiy-grinvich <113624514+serhiy-grinvich@users.noreply.github.com>
2023-01-19 22:21:36 +0200

    add solution

---
+++ lesson8_lifecycle/task1_lifecycle_methods/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Life from './Life';
+// import './index.scss';
+
+const rootElem = document.querySelector('#root');
+
+ReactDOM.render(<Life />, rootElem);
+
+// algo
+//
