2824fe34016b484bc9007501c99070cb3f0ca78c
serhiy-grinvich <113624514+serhiy-grinvich@users.noreply.github.com>
2023-01-23 23:57:18 +0200

    add solution

---
+++ lesson9_forms/task5_game/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './Game';
+import './index.scss';
+
+const rootElem = document.querySelector('#root');
+
+ReactDOM.render(<Game />, rootElem);
