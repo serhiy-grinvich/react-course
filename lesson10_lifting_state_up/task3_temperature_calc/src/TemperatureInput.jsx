b718fa00646e412d7af94d3ed26b63e33ae8cbbc
serhiy-grinvich <113624514+serhiy-grinvich@users.noreply.github.com>
2023-01-27 00:22:38 +0200

    add solution

---
+++ lesson10_lifting_state_up/task3_temperature_calc/src/TemperatureInput.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit',
+};
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleChange = (e) => {
+    this.props.onTemperatureChange(e.target.value);
+  };
+
+  render() {
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Введите градусы по шкале {scaleNames[scale]}:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
+
+export default TemperatureInput;
---
+++ lesson10_lifting_state_up/task3_temperature_calc/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calculator from './Calculator';
+import './index.scss';
+
+const rootElem = document.querySelector('#root');
+
+ReactDOM.render(<Calculator />, rootElem);
