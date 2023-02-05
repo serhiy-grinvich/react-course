9af8dd76a329721369a56c9924f75732de8aec85
serhiy-grinvich <113624514+serhiy-grinvich@users.noreply.github.com>
2023-02-05 14:33:33 +0200

    fix

--- lesson11_update_component/task3_expanded_component/src/App.jsx
+++ lesson11_update_component/task3_expanded_component/src/App.jsx
@@ -2,25 +2,10 @@ import React from 'react';
 import Expand from './Expand';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isExpanded: false,
-    };
-  }
-
-  toggleExpand = () => {
-    this.setState({ isExpanded: !this.state.isExpanded });
-  };
-
   render() {
     return (
       <div className="app">
-        <Expand
-          title="Some title"
-          isExpanded={this.state.isExpanded}
-          onChange={this.toggleExpand}
-        >
+        <Expand title="Some title">
           <div className="expand__content">
             <p>
               Hooks are a new addition in React 16.8. They let you use state and
--- lesson11_update_component/task3_expanded_component/src/Expand.jsx
+++ lesson11_update_component/task3_expanded_component/src/Expand.jsx
@@ -1,32 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Expand = ({ isExpanded, onChange, title, children }) => {
-  return (
-    <div className="expand border">
-      <div className="expand__header">
-        <span className="expand__title">{title}</span>
-        <button className="expand__toggle-btn" onClick={onChange}>
-          <i
-            className={isExpanded ? 'fas fa-chevron-up' : 'fas fa-chevron-down'}
-          ></i>
-          {/* <FontAwesomeIcon icon="fas fa-chevron-up" /> */}
-        </button>
+class Expand extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpanded: false,
+    };
+  }
+
+  toggleExpand = () => {
+    this.setState({ isExpanded: !this.state.isExpanded });
+  };
+
+  render() {
+    const { onChange, title, children } = this.props;
+    const isExpanded = this.state.isExpanded;
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{title}</span>
+          <button className="expand__toggle-btn" onClick={this.toggleExpand}>
+            <i
+              className={
+                isExpanded ? 'fas fa-chevron-up' : 'fas fa-chevron-down'
+              }
+            ></i>
+            {/* <FontAwesomeIcon icon="fas fa-chevron-up" /> */}
+          </button>
+        </div>
+        {isExpanded ? children : null}
       </div>
-      {isExpanded ? children : null}
-    </div>
-  );
-};
+    );
+  }
+}
 
 Expand.propTypes = {
-  isExpanded: PropTypes.bool,
-  onChange: PropTypes.func.isRequired,
   title: PropTypes.string,
   children: PropTypes.element.isRequired,
 };
 
 Expand.defaultProps = {
-  isExpanded: false,
   title: '',
 };
 
