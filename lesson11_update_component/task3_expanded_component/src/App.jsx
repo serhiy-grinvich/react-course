5c20e3a3692e11678b3f70a4db1f214cb5943f90
serhiy-grinvich <113624514+serhiy-grinvich@users.noreply.github.com>
2023-02-05 11:28:09 +0200

    add solution

---
+++ lesson11_update_component/task3_expanded_component/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Expand from './Expand';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpanded: false,
+    };
+  }
+
+  toggleExpand = () => {
+    this.setState({ isExpanded: !this.state.isExpanded });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <Expand
+          title="Some title"
+          isExpanded={this.state.isExpanded}
+          onChange={this.toggleExpand}
+        >
+          <p>
+            Hooks are a new addition in React 16.8. They let you use state and
+            other React features without writing a class.
+          </p>
+        </Expand>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ lesson11_update_component/task3_expanded_component/src/Expand.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Expand = ({ isExpanded, onChange, title, children }) => {
+  return (
+    <div className="expand border">
+      <div className="expand__header">
+        <span className="expand__title">{title}</span>
+        <button className="expand__toggle-btn" onClick={onChange}>
+          <i
+            className={isExpanded ? 'fas fa-chevron-up' : 'fas fa-chevron-down'}
+          ></i>
+          {/* <FontAwesomeIcon icon="fas fa-chevron-up" /> */}
+        </button>
+      </div>
+      <div className="expand__content">{isExpanded ? children : null}</div>
+    </div>
+  );
+};
+
+Expand.propTypes = {
+  isExpanded: PropTypes.bool,
+  onChange: PropTypes.func.isRequired,
+  title: PropTypes.string,
+  children: PropTypes.element.isRequired,
+};
+
+Expand.defaultProps = {
+  isExpanded: false,
+  title: '',
+};
+
+export default Expand;
