9e1ba188e373753f081eb4a2635bacd0f8346538
serhiy-grinvich <113624514+serhiy-grinvich@users.noreply.github.com>
2023-03-08 14:20:51 +0200

    add solution

--- lesson13_router/task3_nested_routes-users/src/User.jsx
+++ lesson13_router/task3_nested_routes-users/src/User.jsx
@@ -29,7 +29,7 @@ class User extends React.Component {
   //     console.log('unmount');
   //   }
   render() {
-    // console.log('render');
+    console.log('render');
 
     const { name, location, avatar_url } = this.state.user;
     return (
